6a4d32c648761edc22957149f4137472958a98d4
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-09-07 20:18:56 +0300

    add models.py

--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,61 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        ordering = ("username",)
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("cafe:cook-detail", kwargs={"pk": self.pk})
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                   related_name="dishes")
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "dish"
+        verbose_name_plural = "dishes"
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    dishes = models.ManyToManyField(
+        Dish,
+        on_delete=models.CASCADE,
+        related_name="ingredients"
+    )
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
