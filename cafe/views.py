c0dc24d908b7781538f4c0421abab3f94be881f9
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-09-08 16:11:01 +0300

    add cafe.urls.py, add index for views.py, add index.html & base.html

--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from cafe.models import Cook, Dish, DishType
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "cafe/index.html", context=context)
