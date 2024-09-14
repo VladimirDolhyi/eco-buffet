2cba7ee73c17271d8fe8a1468f3b5063600cd5f1
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-09-14 19:02:23 +0300

    add login.html & logout.html

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from cafe.views import index
 from cafe.views import (
+    index,
+toggle_assign_to_dish,
     DishTypeListView,
     IngredientListView,
     DishListView,
+    DishDetailView,
     CookListView,
+    CookDetailView,
 )
 
 urlpatterns = [
@@ -13,11 +16,11 @@ urlpatterns = [
     path("dishtypes/", DishTypeListView.as_view(), name="dish-type-list"),
     path("ingredients/", IngredientListView.as_view(), name="ingredient-list"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
-    # path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/<int:pk>/toggle-assign/",
+         toggle_assign_to_dish, name="toggle-dish-assign",),
     path("cooks/", CookListView.as_view(), name="cook-list"),
-    # path(
-    #     "cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"
-    # ),
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
 ]
 
 app_name = "cafe"
--- cafe/views.py
+++ cafe/views.py
@@ -1,10 +1,14 @@
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from cafe.models import Cook, Dish, DishType, Ingredient
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
 
@@ -25,14 +29,14 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "cafe/index.html", context=context)
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     context_object_name = "dish_type_list"
     template_name = "cafe/dish_type_list.html"
     paginate_by = 5
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     paginate_by = 5
 
@@ -41,16 +45,35 @@ class DishListView(generic.ListView):
         return queryset
 
 
-class IngredientListView(generic.ListView):
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Dish
+
+
+class IngredientListView(LoginRequiredMixin, generic.ListView):
     model = Ingredient
     paginate_by = 5
-    queryset = Ingredient.objects.prefetch_related("dishes")
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     paginate_by = 5
 
     def get_queryset(self):
         queryset = Cook.objects.all()
         return queryset
+
+
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Cook
+
+
+@login_required
+def toggle_assign_to_dish(request, pk):
+    cook = Cook.objects.get(id=request.user.id)
+    if (
+        Dish.objects.get(id=pk) in cook.dishes.all()
+    ):  # probably could check if dish exists
+        cook.dishes.remove(pk)
+    else:
+        cook.dishes.add(pk)
+    return HttpResponseRedirect(reverse_lazy("cafe:dish-detail", args=[pk]))
