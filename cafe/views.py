f0861b053b90cafb57c1677cbc8b5d100a2b62a1
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-11-02 04:19:46 +0200

    add num_ingredients to views.py def index

--- cafe/views.py
+++ cafe/views.py
@@ -24,6 +24,7 @@ def index(request: HttpRequest) -> HttpResponse:
     num_cooks = Cook.objects.count()
     num_dishes = Dish.objects.count()
     num_dish_types = DishType.objects.count()
+    num_ingredients = Ingredient.objects.count()
 
     num_visits = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits + 1
@@ -32,6 +33,7 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_cooks": num_cooks,
         "num_dishes": num_dishes,
         "num_dish_types": num_dish_types,
+        "num_ingredients": num_ingredients,
         "num_visits": num_visits + 1,
     }
 
